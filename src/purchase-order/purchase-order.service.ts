6d11e3a164c6ff22b43914c726af42fcd3e2a7eb
wilson Neto <wngaspar10@gmail.com>
2023-02-09 10:16:48 -0300

    feat: create purchase order service

---
+++ src/purchase-order/purchase-order.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PurchaseOrderDto } from './purchase-order-dto';
+import { PurchaseOrder } from './purchase-order.entity';
+import { PurchaseOrderRepository } from './purchase-order.repository';
+
+@Injectable()
+export class PurchaseOrderService {
+  constructor(private purchaseOrderRepository: PurchaseOrderRepository) {}
+
+  async createPurchaseOrder(
+    createPurchaseOrder: PurchaseOrderDto,
+  ): Promise<PurchaseOrder> {
+    return this.purchaseOrderRepository.createPurchaseOrder(
+      createPurchaseOrder,
+    );
+  }
+}
