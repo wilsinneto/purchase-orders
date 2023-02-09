efa8608d52ed573f8fce4224c69f6139d8f80ef6
wilson Neto <wngaspar10@gmail.com>
2023-02-09 10:16:59 -0300

    feat: create purchase order controller

---
+++ src/purchase-order/purchase-order.controller.ts
@@ -0,0 +1,21 @@
+import { Controller } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common/decorators';
+import { PurchaseOrderDto } from './purchase-order-dto';
+import { PurchaseOrder } from './purchase-order.entity';
+import { PurchaseOrderService } from './purchase-order.service';
+
+@Controller('purchase-orders')
+export class PurchaseOrderController {
+  constructor(private purchaseOrderService: PurchaseOrderService) {}
+
+  @Post()
+  async createPurchaseOrder(
+    @Body() createPurchaseOrder: PurchaseOrderDto,
+  ): Promise<PurchaseOrder> {
+    const purchaseOrder = await this.purchaseOrderService.createPurchaseOrder(
+      createPurchaseOrder,
+    );
+
+    return purchaseOrder;
+  }
+}
