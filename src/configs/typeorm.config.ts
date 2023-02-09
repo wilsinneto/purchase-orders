ee4f4d3a7779ff6bac9b36dacecd456a5bc444e8
wilson Neto <wngaspar10@gmail.com>
2023-02-09 10:15:55 -0300

    feat: created purchase order entity

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,4 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { PurchaseOrder } from 'src/purchase-order/purchase-order.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -7,6 +8,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: 'pguser',
   password: 'pgpassword',
   database: 'pguser',
-  entities: [],
+  entities: [PurchaseOrder],
   synchronize: true,
 };
---
+++ src/purchase-order/purchase-order.entity.ts
@@ -0,0 +1,26 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class PurchaseOrder extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  name: string;
+
+  @Column({ nullable: false, type: 'text' })
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
